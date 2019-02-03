97ef251b95ef981745b5b7157010a82bf08f2d94
Jason Graham <jgraham@compukix.net>
2019-02-03 17:09:26 -0500

    Included the <linux/sched.h> header which is now required.

--- scull/access.c
+++ scull/access.c
@@ -25,6 +25,7 @@
 #include <linux/types.h>  /* size_t */
 #include <linux/fcntl.h>
 #include <linux/cdev.h>
+#include <linux/sched.h>
 #include <linux/tty.h>
 #include <asm/atomic.h>
 #include <linux/list.h>
